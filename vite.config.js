8e581bbd89fd9a3b9b5ecbe5f79ecdbb1c107324
Atul Ambore <atul.ambore@gmail.com>
2025-12-29 00:50:27 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://<github-username>.github.io/<repo-name>/
+// https://AtulAmbore.github.io/agency-ai/
+/*
+You don’t “get” this from anywhere —
+👉 you construct it yourself from:
+Your GitHub username
+Your repository name
+*/
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/agency-ai/",
 });
